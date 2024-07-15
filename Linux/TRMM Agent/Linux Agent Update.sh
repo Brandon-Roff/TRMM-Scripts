aab9f7a5b6e9aa592d60da1147d698fd07443e1f
brandon-roff <brandon-roff@roftwares>
2024-07-15 14:00:39 +0100

    Tidyied up Tactical RMM linux Update Script and included output for All suported architectures

--- Linux/TRMM Agent/Linux Agent Update.sh
+++ Linux/TRMM Agent/Linux Agent Update.sh
@@ -11,30 +11,26 @@ if [ "$ARCH" = "x86_64" ]; then
         ARCH="amd64" 
         # Download Linux RMM Script if not already exists
         if [ ! -f /opt/rmmagent-linux.sh ]; then
-            wget -O /opt/rmmagent-linux.sh https://raw.githubusercontent.com/netvolt/LinuxRMM-Script/main/rmmagent-linux.sh
+            print "Begining the update for a x86_64 (32-Bit) client..."
+            wget -O /opt/rmmagent-linux.sh https://raw.githubusercontent.com/netvolt/LinuxRMM-Script/main/rmmagent-linux.sh  | bash /opt/rmmagent-linux.sh update $ARCH
         fi
-        #run the update script
-        bash /opt/rmmagent-linux.sh update $ARCH
-
 # ARM 64
     elif [ "$ARCH" = "aarch64" ]; then
         ARCH="arm64"
         # Download Linux RMM Script if not already exists
         if [ ! -f /opt/rmmagent-linux.sh ]; then
-            wget -O /opt/rmmagent-linux.sh https://raw.githubusercontent.com/netvolt/LinuxRMM-Script/main/rmmagent-linux.sh
-        fi
-        #run the update script
-        bash /opt/rmmagent-linux.sh update $ARCH        
+            print "Begining the update for a ARM 64-Bit client..."
+            wget -O /opt/rmmagent-linux.sh https://raw.githubusercontent.com/netvolt/LinuxRMM-Script/main/rmmagent-linux.sh | bash /opt/rmmagent-linux.sh update $ARCH
+        fi       
 
 # Raspberry Pi 3 and 4
     elif [ "$ARCH" = "armv7l" ]; then
         ARCH="armv6"
         # Download Linux RMM Script if not already exists
         if [ ! -f /opt/rmmagent-linux.sh ]; then
-            wget -O /opt/rmmagent-linux.sh https://raw.githubusercontent.com/netvolt/LinuxRMM-Script/main/rmmagent-linux.sh
+            print "Begining the update for an ARM 32-bit Client..."
+            wget -O /opt/rmmagent-linux.sh https://raw.githubusercontent.com/netvolt/LinuxRMM-Script/main/rmmagent-linux.sh | bash /opt/rmmagent-linux.sh update $ARCH
         fi
-        #run the update script
-        bash /opt/rmmagent-linux.sh update $ARCH
 
     else
         echo "Unsupported architecture"
